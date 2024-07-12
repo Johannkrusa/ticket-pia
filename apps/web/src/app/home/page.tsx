5bfac925d25306e95d07e885e0923ecd7a4104de
“chrdesmond” <cddesmon120@gmail.com>
2024-07-12 16:24:52 +0700

    feat(front-end): add Header & Footer

---
+++ apps/web/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+    return(
+        <>
+            <div className="bg-blue-500 w-full h-[3000px]">
+                halo
+            </div>
+        </>
+    )
+}
